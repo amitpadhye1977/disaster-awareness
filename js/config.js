96c77ef6408f97acd5cde82bf8c7d6d09bed1fff
amitpadhye1977 <amit.padhye@gmail.com>
2026-08-07 08:27:13 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,18 +1,24 @@
 const APP = {
 
-title:
-"Disaster Preparedness Awareness Initiative",
+    title:
+    "Disaster Preparedness Awareness Initiative",
 
-tagline:
-"Creating Safer Homes Through Awareness",
+    tagline:
+    "Creating Safer Homes Through Awareness",
 
-preparedBy:
-"Amit Padhye",
+    preparedBy:
+    "Amit Padhye",
 
-course:
-"Certificate Program in Disaster Management Law",
+    course:
+    "Certificate Program in Disaster Management Law",
 
-college:
-"VPM's TMC Law College, Thane"
+    college:
+    "VPM's TMC Law College, Thane",
+
+    version:
+    "1.0",
+
+    googleScriptUrl:
+    "https://script.google.com/macros/s/AKfycbwUSe3Lgtt9Js_rI_T5CNALFqRZmYmh4HE9o6gkvdnri5NmWTT7SNJYnO2-4wN-bDCE/exec"
 
 };
